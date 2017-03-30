e6a40555810e5316063e89eb6c98111202297539
Carlos Staszeski <cstaszeski@gmail.com>
2017-03-30 12:05:59 -0700

    Set up pug (formerly jade) view engine and routed / to render index.pug

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
 
 app.listen(3000, () => {
 	console.log('Listening at 192.168.83.128:3000...');
